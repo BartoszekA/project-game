84c0bd0a0483ea244ca35b9101902c6c074fc143
BartoszekA <agnieszka.bartoszek@gmail.com>
2019-09-10 12:11:07 +0200

    next commit

--- game/src/main/java/com/kodilla/game/TicTacToeLogic.java
+++ game/src/main/java/com/kodilla/game/TicTacToeLogic.java
@@ -1,40 +1,40 @@
 package com.kodilla.game;
 
-import java.util.ArrayList;
-
 public class TicTacToeLogic {
 
-    public ArrayList<Symbol> nextMove(ArrayList<Symbol> board, int place, Symbol symbol) {
-        if (board.get(place) instanceof Blank) {
-            board.set(place, symbol);
+    public Board nextMove(Board board, int place, Symbol symbol) {
+        if (board.getBoard().get(place) instanceof Blank) {
+            board.getBoard().set(place, symbol);
         }
         return board;
     }
 
-    public boolean checkIfVictory(ArrayList<Symbol> board) {
+    public boolean checkIfVictory(Board board) {
 
-        return (board.get(0) instanceof Cross && board.get(1) instanceof Cross && board.get(2) instanceof Cross) ||
-                (board.get(3) instanceof Cross && board.get(4) instanceof Cross && board.get(5) instanceof Cross) ||
-                (board.get(6) instanceof Cross && board.get(7) instanceof Cross && board.get(2) instanceof Cross) ||
-                (board.get(0) instanceof Cross && board.get(3) instanceof Cross && board.get(6) instanceof Cross) ||
-                (board.get(1) instanceof Cross && board.get(4) instanceof Cross && board.get(7) instanceof Cross) ||
-                (board.get(2) instanceof Cross && board.get(5) instanceof Cross && board.get(8) instanceof Cross) ||
-                (board.get(0) instanceof Cross && board.get(4) instanceof Cross && board.get(8) instanceof Cross) ||
-                (board.get(2) instanceof Cross && board.get(4) instanceof Cross && board.get(6) instanceof Cross) ||
+        return (board.getBoard().get(0) instanceof Cross && board.getBoard().get(1) instanceof Cross && board.getBoard().get(2) instanceof Cross) ||
+                (board.getBoard().get(3) instanceof Cross && board.getBoard().get(4) instanceof Cross && board.getBoard().get(5) instanceof Cross) ||
+                (board.getBoard().get(6) instanceof Cross && board.getBoard().get(7) instanceof Cross && board.getBoard().get(2) instanceof Cross) ||
+                (board.getBoard().get(0) instanceof Cross && board.getBoard().get(3) instanceof Cross && board.getBoard().get(6) instanceof Cross) ||
+                (board.getBoard().get(1) instanceof Cross && board.getBoard().get(4) instanceof Cross && board.getBoard().get(7) instanceof Cross) ||
+                (board.getBoard().get(2) instanceof Cross && board.getBoard().get(5) instanceof Cross && board.getBoard().get(8) instanceof Cross) ||
+                (board.getBoard().get(0) instanceof Cross && board.getBoard().get(4) instanceof Cross && board.getBoard().get(8) instanceof Cross) ||
+                (board.getBoard().get(2) instanceof Cross && board.getBoard().get(4) instanceof Cross && board.getBoard().get(6) instanceof Cross) ||
 
-                (board.get(0) instanceof Circle && board.get(1) instanceof Circle && board.get(2) instanceof Circle) ||
-                (board.get(3) instanceof Circle && board.get(4) instanceof Circle && board.get(5) instanceof Circle) ||
-                (board.get(6) instanceof Circle && board.get(7) instanceof Circle && board.get(2) instanceof Circle) ||
-                (board.get(0) instanceof Circle && board.get(3) instanceof Circle && board.get(6) instanceof Circle) ||
-                (board.get(1) instanceof Circle && board.get(4) instanceof Circle && board.get(7) instanceof Circle) ||
-                (board.get(2) instanceof Circle && board.get(5) instanceof Circle && board.get(8) instanceof Circle) ||
-                (board.get(0) instanceof Circle && board.get(4) instanceof Circle && board.get(8) instanceof Circle) ||
-                (board.get(2) instanceof Circle && board.get(4) instanceof Circle && board.get(6) instanceof Circle);
+                (board.getBoard().get(0) instanceof Circle && board.getBoard().get(1) instanceof Circle && board.getBoard().get(2) instanceof Circle) ||
+                (board.getBoard().get(3) instanceof Circle && board.getBoard().get(4) instanceof Circle && board.getBoard().get(5) instanceof Circle) ||
+                (board.getBoard().get(6) instanceof Circle && board.getBoard().get(7) instanceof Circle && board.getBoard().get(2) instanceof Circle) ||
+                (board.getBoard().get(0) instanceof Circle && board.getBoard().get(3) instanceof Circle && board.getBoard().get(6) instanceof Circle) ||
+                (board.getBoard().get(1) instanceof Circle && board.getBoard().get(4) instanceof Circle && board.getBoard().get(7) instanceof Circle) ||
+                (board.getBoard().get(2) instanceof Circle && board.getBoard().get(5) instanceof Circle && board.getBoard().get(8) instanceof Circle) ||
+                (board.getBoard().get(0) instanceof Circle && board.getBoard().get(4) instanceof Circle && board.getBoard().get(8) instanceof Circle) ||
+                (board.getBoard().get(2) instanceof Circle && board.getBoard().get(4) instanceof Circle && board.getBoard().get(6) instanceof Circle);
+
+
 
     }
 
-    public boolean checkLoss(ArrayList<Symbol> board) {
-        for(Symbol item : board) {
+    public boolean checkLoss(Board board) {
+        for(Symbol item : board.getBoard()) {
             if (item instanceof Blank) return true;
         }
         return false;
--- game/src/test/java/com/kodilla/game/GameTestSuite.java
+++ game/src/test/java/com/kodilla/game/GameTestSuite.java
@@ -1,14 +1,20 @@
 package com.kodilla.game;
 
+import org.junit.Assert;
 import org.junit.Test;
-
-import static com.kodilla.game.Board.createBoard;
 
 public class GameTestSuite {
     @Test
     public void testCaseTicTacToe() {
         //Given
-        nextMove(createBoard(), 0, new Circle());
+        TicTacToeLogic logic = new TicTacToeLogic();
+        Board myBoard = Board.createBoard();
+        logic.nextMove(myBoard, 0, new Circle());
         //When
+        boolean firstCheck = logic.checkIfVictory(myBoard);
+        boolean secondCheck = logic.checkLoss(myBoard);
+        //Then
+        Assert.assertFalse(firstCheck);
+        Assert.assertTrue(secondCheck);
     }
 }
